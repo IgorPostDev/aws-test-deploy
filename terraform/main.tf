ef45c27baf65d3d99507aa3547bcc01deedc8a97
Igorengidev <ihor.postavnychyi@kozaktex.com>
2024-06-26 18:06:52 +0300

    provider aws {

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,21 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.aws_region}a"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "${var.aws_region}b"
 }
 
 resource "aws_internet_gateway" "main" {
@@ -24,8 +31,13 @@ resource "aws_route_table" "main" {
   }
 }
 
-resource "aws_route_table_association" "main" {
-  subnet_id      = aws_subnet.main.id
+resource "aws_route_table_association" "subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route_table_association" "subnet2" {
+  subnet_id      = aws_subnet.subnet2.id
   route_table_id = aws_route_table.main.id
 }
 
@@ -40,8 +52,8 @@ resource "aws_security_group" "app_sg" {
   }
 
   ingress {
-    from_port   = 3306
-    to_port     = 3306
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -54,42 +66,54 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
-resource "aws_db_instance" "db" {
-  allocated_storage    = 20
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t2.micro"
-  username             = "laravel"
-  password             = "secret"
-  parameter_group_name = "default.mysql8.0"
-  skip_final_snapshot  = true
-
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
-
-  db_subnet_group_name = aws_db_subnet_group.main.name
-}
-
-resource "aws_db_subnet_group" "main" {
-  name       = "main"
-  subnet_ids = [aws_subnet.main.id]
-}
-
 resource "aws_instance" "app" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
-  instance_type = "t2.micro"
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              amazon-linux-extras install docker -y
-              service docker start
-              usermod -a -G docker ec2-user
-              docker run -d -p 80:80 --name laravel_app -e DB_HOST=${aws_db_instance.db.address} -e DB_DATABASE=laravel -e DB_USERNAME=laravel -e DB_PASSWORD=secret your-docker-image
-              EOF
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.subnet1.id
+  security_groups        = [aws_security_group.app_sg.name]
+  associate_public_ip_address = true
 
   tags = {
     Name = "LaravelApp"
   }
 
-  security_groups = [aws_security_group.app_sg.name]
+  user_data = <<-EOF
+              #!/bin/bash
+              docker run -d -p 80:80 ${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/aws-deployment-example:latest
+              EOF
 }
+
+resource "aws_db_subnet_group" "main" {
+  name       = "main"
+  subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+}
+
+resource "aws_db_instance" "db" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t2.micro"
+  username             = "admin"
+  password             = "password"
+  db_subnet_group_name = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  skip_final_snapshot  = true
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "ami" {
+  default = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "aws_account_id" {
+  type = string
+}
+
